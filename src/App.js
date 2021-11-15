c9b6c0488c5e53ec6dbac6a98b879d78e4d477e7
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-11-15 11:52:27 +0100

    Aad stats

--- src/App.js
+++ src/App.js
@@ -1,47 +1,70 @@
 import './App.css'
 
-const points = [8, 4, 10, 34, 7];
-const rebounds = null
-const assists = null
+const stats = {
+  '2020-2021': {
+    points: [8, 4, 10, 34, 7],
+    rebounds: null,
+    assists: null
+  },
 
-const addStatsColumns = points.map((day, index) => {
+  '2021-2022': {
+    points: [19, 9, 9, 18, 25, 9],
+    rebounds: ['-', '-', '-', 1, 7, 5],
+    assists: ['-', '-', 6, 5, 13, 7]
+  }
+}
+
+const addStatsColumns = (seasonStats) => seasonStats.points.map((day, index) => {
   return (
-    <><div class='app-grid__points'>
-      {points[index]}
+    <>
+    <div className='app-grid__points'>
+      {seasonStats.points[index]}
+    </div>
+      <div className='app-grid__rebounds'>
+        {seasonStats.rebounds ? seasonStats.rebounds[index] : '-'}
       </div>
-      <div class='app-grid__rebounds'>
-        {rebounds ? rebounds[index] : '-'}
-      </div>
-      <div class='app-grid__assists'>
-        {assists ? assists[index] : '-'}
+      <div className='app-grid__assists'>
+        {seasonStats.assists ? seasonStats.assists[index] : '-'}
       </div>
     </>
   )
 })
 
-const calculateAverage = (arr) => arr ? (arr.reduce((total, number) => {
-  return total + number;
-}, 0) / arr.length).toFixed(2) : '-';
+const calculateAverage = (arr) => arr
+  ? (arr.reduce((total, number) => {
+      return number !== '-' ? total + number : total
+    }, 0) / arr.length).toFixed(2)
+  : '-'
 
 const App = () => {
   return (
-    <div class='app-grid'>
-      <div class='app-grid__player-box'>Boubou</div>
-      <div class='app-grid__points header'>
-        POINTS
-      </div>
-      <div class='app-grid__rebounds header'>
-        REBOUNDS
-      </div>
-      <div class='app-grid__assists header'>
-        ASSISTS
-      </div>
+    <div className='app-grid'>
+      <div className='app-grid__player-box'>Boubou</div>
 
-      {addStatsColumns}
+      {Object.keys(stats).map(season => {
+        const seasonStats = stats[season]
+        return (
+          <>
+            <div className='app-grid__season'>{season}</div>
+            <div className='app-grid__points header'>
+              POINTS
+            </div>
+            <div className='app-grid__rebounds header'>
+              REBOUNDS
+            </div>
+            <div className='app-grid__assists header'>
+              ASSISTS
+            </div>
 
-      <div class='app-grid__points_total'>{calculateAverage(points)}</div>
-      <div class='app-grid__rebounds_total'>{calculateAverage(rebounds)}</div>
-      <div class='app-grid__assists_total'>{calculateAverage(assists)}</div>
+            {addStatsColumns(seasonStats)}
+
+            <div className='app-grid__points_total'>{calculateAverage(seasonStats.points)}</div>
+            <div className='app-grid__rebounds_total'>{calculateAverage(seasonStats.rebounds)}</div>
+            <div className='app-grid__assists_total'>{calculateAverage(seasonStats.assists)}</div>
+          </>
+        )
+      })}
+
     </div>
   )
 }

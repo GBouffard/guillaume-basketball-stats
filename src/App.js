62cff3d5a467e1ee60024a04e30045f466779262
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-11-15 11:08:31 +0100

    Add first stats and style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import './App.css'
 
+const points = [8, 4, 10, 34, 7];
+const rebounds = null
+const assists = null
+
+const addStatsColumns = points.map((day, index) => {
+  return (
+    <><div class='app-grid__points'>
+      {points[index]}
+      </div>
+      <div class='app-grid__rebounds'>
+        {rebounds ? rebounds[index] : '-'}
+      </div>
+      <div class='app-grid__assists'>
+        {assists ? assists[index] : '-'}
+      </div>
+    </>
+  )
+})
+
+const calculateAverage = (arr) => arr ? (arr.reduce((total, number) => {
+  return total + number;
+}, 0) / arr.length).toFixed(2) : '-';
+
 const App = () => {
-  const points = [8, 4, 10, 34, 7];
-  const rebounds = null
-  const assists = null
-
-  const addStatsColumns = points.map((day, index) => {
-    return (
-      <><div class='app-grid__points'>
-        {points[index]}
-        </div>
-        <div class='app-grid__rebounds'>
-          {rebounds ? rebounds[index] : '-'}
-        </div>
-        <div class='app-grid__assists'>
-          {assists ? assists[index] : '-'}
-        </div>
-      </>
-    )
-  })
-
   return (
     <div class='app-grid'>
       <div class='app-grid__player-box'>Boubou</div>
-      <div class='app-grid__points'>
+      <div class='app-grid__points header'>
         POINTS
       </div>
-      <div class='app-grid__rebounds'>
+      <div class='app-grid__rebounds header'>
         REBOUNDS
       </div>
-      <div class='app-grid__assists'>
+      <div class='app-grid__assists header'>
         ASSISTS
       </div>
 
       {addStatsColumns}
 
-      <div class='app-grid__points_total' />
-      <div class='app-grid__rebounds_total' />
-      <div class='app-grid__assists_total' />
+      <div class='app-grid__points_total'>{calculateAverage(points)}</div>
+      <div class='app-grid__rebounds_total'>{calculateAverage(rebounds)}</div>
+      <div class='app-grid__assists_total'>{calculateAverage(assists)}</div>
     </div>
   )
 }

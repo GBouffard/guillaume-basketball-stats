32cb1b3b3a39a81df7b77753bfc56e54cdfb6e42
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2021-11-15 15:13:28 +0100

    Add style

--- src/imagesUrls.js
+++ src/imagesUrls.js
@@ -2,7 +2,7 @@ const baseUrl = process.env.PUBLIC_URL;
 
 const imagesUrls = {
   boubou: `${baseUrl}/images/Boubou.jpg`,
-  flag: `${baseUrl}/images/France.jpg`
+  flag: `${baseUrl}/images/France.png`
 };
 
 export default imagesUrls;

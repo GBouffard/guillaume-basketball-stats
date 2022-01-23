658c2570303dd98c1641c28c1efc6d2471011b65
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2022-01-23 11:56:46 +0100

    Allow click through on games

--- src/AllGamesStats.js
+++ src/AllGamesStats.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const AllGamesStats = ({ seasonStats }) => <div className='app-grid__games-table'>
   {seasonStats.points.map((day, index) => (
-  <>
-    <div className='app-grid__points'>
-      {seasonStats.points[index]}
-    </div>
-    <div className='app-grid__rebounds'>
-      {seasonStats.rebounds ? seasonStats.rebounds[index] : '-'}
-    </div>
-    <div className='app-grid__assists'>
-      {seasonStats.assists ? seasonStats.assists[index] : '-'}
-    </div>
-  </>))}
-  </div>
+    <>
+      <div className='app-grid__points'>
+        {seasonStats.points[index]}
+      </div>
+      <div className='app-grid__rebounds'>
+        {seasonStats.rebounds ? seasonStats.rebounds[index] : '-'}
+      </div>
+      <div className='app-grid__assists'>
+        {seasonStats.assists ? seasonStats.assists[index] : '-'}
+      </div>
+    </>))}
+</div>
 
 export default AllGamesStats

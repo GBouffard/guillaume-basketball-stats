1061fd27af52cd0f6dff1ae80aa0f42db87c931a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2022-01-24 10:13:37 +0100

    Scroll down to bottom in games

--- src/AllGamesStats.js
+++ src/AllGamesStats.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const AllGamesStats = ({ seasonStats }) => <div className='app-grid__games-table'>
-  {seasonStats.points.map((day, index) => (
-    <>
-      <div className='app-grid__points'>
-        {seasonStats.points[index]}
+const scrollToBottom = () => {
+  const myDiv = document.getElementById('scrollable-table')
+  myDiv.scrollTop = myDiv.scrollHeight
+}
+
+class AllGamesStats extends Component {
+  componentDidMount () {
+    scrollToBottom()
+  }
+
+  componentDidUpdate () {
+    scrollToBottom()
+  }
+
+  render () {
+    const { seasonStats } = this.props
+
+    return (
+      <div className='app-grid__games-table' id='scrollable-table'>
+        {seasonStats.points.map((day, index) => (
+          <>
+            <div className='app-grid__points'>
+              {seasonStats.points[index]}
+            </div>
+            <div className='app-grid__rebounds'>
+              {seasonStats.rebounds ? seasonStats.rebounds[index] : '-'}
+            </div>
+            <div className='app-grid__assists'>
+              {seasonStats.assists ? seasonStats.assists[index] : '-'}
+            </div>
+          </>))}
       </div>
-      <div className='app-grid__rebounds'>
-        {seasonStats.rebounds ? seasonStats.rebounds[index] : '-'}
-      </div>
-      <div className='app-grid__assists'>
-        {seasonStats.assists ? seasonStats.assists[index] : '-'}
-      </div>
-    </>))}
-</div>
+    )
+  }
+}
 
 export default AllGamesStats
